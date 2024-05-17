3ff23d23510159ae8a111a7951664217c9e76976
PythonMonty <andrewnovosel90@gmail.com>
2024-05-17 11:29:41 -0400

    Attempting to create sidebar

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function w3_open() {
-  document.getElementById("mySidebar").style.width = "100%";
-  document.getElementById("mySidebar").style.display = "block";
+function openSidebar() {
+  document.getElementById("sideBar").classList.add("show");
 }
 
-function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
+function closeSidebar() {
+  document.getElementById("sideBar").classList.remove("show");
 }
